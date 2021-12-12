b0f408d02686db7428d4837dc91f9c992c8eb37a
jianguo-dou <13681947410@163.com>
2021-12-12 22:54:36 +0800

    add batchStartJar.sh

---
+++ command/src/main/java/batchStartJar.sh
@@ -0,0 +1,15 @@
+cd /
+cd /Users/doujianguo/FEBS-Cloud/febs-tx-manager/target
+nohup java -jar febs-tx-manager-2.2-RELEASE.jar &
+cd /
+cd /Users/doujianguo/FEBS-Cloud/febs-apm/febs-admin/target
+nohup java -jar febs-admin-2.2-RELEASE.jar &
+cd /
+cd /Users/doujianguo/FEBS-Cloud/febs-auth/target
+nohup java -jar febs-auth-2.2-RELEASE.jar &
+cd /
+cd /Users/doujianguo/FEBS-Cloud/febs-server/febs-server-system/target
+nohup java -jar febs-server-system-2.2-RELEASE.jar &
+cd /
+cd /Users/doujianguo/FEBS-Cloud/febs-gateway/target
+nohup java -jar febs-gateway-2.2-RELEASE.jar &
